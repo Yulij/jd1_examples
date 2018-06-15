3e35955ed64b46e755d516892ffed71ce553b58b
yslabko <yslabko@exadel.com>
2018-06-15 17:15:19 +0300

    Update synhro test

--- src/lecture18_threads/synchro/ThreadTest.java
+++ src/lecture18_threads/synchro/ThreadTest.java
@@ -4,31 +4,23 @@ package lecture18_threads.synchro;
  * Created by yslabko on 016 16.05.16.
  */
 public class ThreadTest {
-    private volatile int a=1;
-    private volatile int b=2;
+    private int a=1;
+    private int b=2;
     public void one() {a=b;}
 
     public void two() {b=a;}
 
     public static void main(String s[]) {
-        int a11=0, a22=0, a12=0;
+        int aOverritten=0, bOverritten=0, abOverritten=0;
         for (int i=0; i<100; i++) {
             final ThreadTest o = new ThreadTest();
             // Запускаем первый поток, который
             // вызывает метод one()
-            new Thread() {
-                public void run() {
-                    o.one();
-                }
-            }.start();
+            new Thread(o::one).start();
 
             // Запускаем второй поток, который
             // вызывает метод two()
-            new Thread() {
-                public void run() {
-                    o.two();
-                }
-            }.start();
+            new Thread(o::two).start();
 
             // даем потокам время отработать
             try {
@@ -36,10 +28,10 @@ public class ThreadTest {
             } catch (InterruptedException e) {}
 
             // анализируем финальные значения
-            if (o.a==1 && o.b==1) a11++;
-            if (o.a==2 && o.b==2) a22++;
-            if (o.a!=o.b) a12++;
+            if (o.a==1 && o.b==1) aOverritten++;
+            if (o.a==2 && o.b==2) bOverritten++;
+            if (o.a!=o.b) abOverritten++;
         }
-        System.out.println(a11+" "+a22+" "+a12);
+        System.out.println("aOverritten:" + aOverritten+" bOverritten:"+bOverritten+" abOverritten:"+abOverritten);
     }
 }
